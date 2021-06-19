45394b9d93a22174fd06e766686b17d2510441ab
Amadou BAH <51505362+Ama-Lion@users.noreply.github.com>
2021-06-19 13:14:15 +0200

    tested welcome screen component

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { View } from "react-native";
 
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
 }
--- app/screens/WelcomeScreen.jsx
+++ app/screens/WelcomeScreen.jsx
@@ -5,7 +5,7 @@ import AppButton from "../components/AppButton";
 function WelcomeScreen(props) {
   return (
     <ImageBackground
-      blurRadius={10}
+      // blurRadius={0}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
