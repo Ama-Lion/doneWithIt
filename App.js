2e3f6458747e2759692591cab9d11c0054060925
Amadou BAH <51505362+Ama-Lion@users.noreply.github.com>
2021-06-19 11:49:57 +0200

    created image view screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Button, SafeAreaView } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
